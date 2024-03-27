736425864cfb06ce7fd8ab7e59f3467f653b60b2
tiaratia <143604612+tiaratia@users.noreply.github.com>
2024-03-27 21:51:03 +0700

    Add files via upload

---
+++ string slice.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#•	STRING SLICE
+text = "pisang goreng"
+a = text[0:6]
+print(a)
+
+
+# In[2]:
+
+
+#•	STRING SLICE
+text = "pisang goreng"
+a = text[-4]
+print(a)
+
+
+# In[ ]:
+
+
+
+
