74d8d387e958be67a611ca730f432ead43f3f59d
tiaratia <143604612+tiaratia@users.noreply.github.com>
2024-03-27 22:12:47 +0700

    Add files via upload

---
+++ operasi kali dan tambah pada String.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#•	STRING SLICE
+text = "pisang goreng"
+a = text[0:6]
+print(a)
+
+
+# In[2]:
+
+
+#•	STRING SLICE
+text = "pisang goreng"
+a = text[-4]
+print(a)
+
+
+# In[5]:
+
+
+# Operator * dan + pada String
+text1 = "kampus"
+text2 = "ukdw"
+text3 = "dijogja"
+text4 = text1 + " " + text2 + " " + text3
+print(text4)
+
+
+# In[6]:
+
+
+text = "UKDW"
+print(text * 1)
+
+
+# In[ ]:
+
+
+
+
